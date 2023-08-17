b2eb892a9f4713dce27e45872e15847f47b1f983
ABDUL GHANI CHHIPA <141896568+octacreativepk@users.noreply.github.com>
2023-08-18 04:51:15 +0500

    Add files via upload

---
+++ assignment/index1.js
@@ -0,0 +1,59 @@
+// //____________________________ ASSIGNMENT 2 __________________
+    // Task 1
+    // var username = "Pakistan";
+    // alert(username)
+
+    // // Task 2
+    // var myName = "Abdul Ghani";
+    // alert(myName)
+
+    // // Task 3
+    // var message = "I am Pakistani.";
+    // alert(message);
+
+    // // Task 4
+    // var studentName = "Abdul Ghani";
+    // var age = 18;
+    // var course = "Computer Science";
+    // var contact = "abdulghanichhipa2531@gmail.com";
+
+    // alert("Student Name: " + studentName);
+    // alert("Age: " + age);
+    // alert("Course: " + course);
+    // alert("Contact: " + contact);
+    // //Task 5
+    // var a = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+    // alert(a)
+    // //Task 6
+    // var emailid = "abdulghanichhipa2531@gmail.com";
+    // alert("My email is: " + emailid)
+    // // Task 7
+    // var book = "A Smarter Book to Learn JS";
+    // alert("I am trying to learn book is: " + book);
+    // // Task 8
+    // var msg="Are You Live in Pakistan ? ";
+    // var outputDiv = document.getElementById("output");
+    // outputDiv.innerHTML += "<p>" + msg + "</p>";
+    // // Task 9
+    // var val="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+    // alert(val)
+
+// //____________________________ ASSIGNMENT 3 ____________________________
+    // // Task 1
+    // var z=18;
+    // var x="I am " +z+ " years old";
+    // alert(x)
+    // // Task 2
+    // var visitCount = localStorage.getItem("visitCount") || 0;
+    // visitCount++;
+    // localStorage.setItem("visitCount", visitCount);
+    // alert("You have visited this site " + visitCount + " times.")
+    // // Task 3
+    // // Task is written in HTML
+    // // Task 4
+    // // Task is written in HTML
+// // ____________________________ASSIGNMENT 4 ____________________________
+// // Tasks are written in HTML
+
+// // ____________________________ASSIGNMENT 5 ____________________________
+// // Tasks are written in HTML
